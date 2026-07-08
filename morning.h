7a0c0d2487e4cb90208d8b4e7b5833d7a4d01e0e
YOSHIDA Takehiko <takehiko.yoshida@mimaki.com>
2026-07-08 10:31:47 +0000

    morning.hとnight.hにインクルードガードを変更

--- morning.h
+++ morning.h
@@ -1,3 +1,8 @@
+#ifndef MORNING_H
+#define MORNING_H
+
 #include "greeting.h"
 
 ErrorCode morning_greeting(const char* person_name);
+
+#endif /* MORNING_H */
--- night.h
+++ night.h
@@ -1,3 +1,8 @@
+#ifndef NIGHT_H
+#define NIGHT_H
+
 #include "greeting.h"
 
 ErrorCode night_greeting(const char* person_name);
+
+#endif /* NIGHT_H */
