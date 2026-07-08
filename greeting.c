0c03fc5f65fe8e474ff8ce03ccca8425509fc0fe
YOSHIDA Takehiko <takehiko.yoshida@mimaki.com>
2026-07-08 10:23:45 +0000

    greeting.hに宣言の記述を分離

--- greeting.c
+++ greeting.c
@@ -1,10 +1,5 @@
+#include "greeting.h"
 #include <stdio.h>
-
-typedef enum ErrorCode {
-    ERROR_NONE      = 0x00000000,
-    ERROR_ARG       = 0x00000001,
-    ERROR_UNDEFINED = 0xFFFFFFFF
-} ErrorCode;
 
 ErrorCode greeting(const char* period_name,const char* person_name)
 {
--- morning.c
+++ morning.c
@@ -1,10 +1,4 @@
-typedef enum ErrorCode {
-    ERROR_NONE      = 0x00000000,
-    ERROR_ARG       = 0x00000001,
-    ERROR_UNDEFINED = 0xFFFFFFFF
-} ErrorCode;
-
-ErrorCode greeting(const char* period_name, const char* person_name);
+#include "greeting.h"
 
 ErrorCode morning_greeting(const char* person_name)
 {
